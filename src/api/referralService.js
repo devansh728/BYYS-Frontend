bb86ab7d3d4c6b9418c990818f77bd361568e3c7
devansh728 <gargdevansh728@gmail.com>
2025-08-27 20:16:18 +0530

    hello

--- src/api/referralService.js
+++ src/api/referralService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.VITE_API_BASE_URL;
+const API_BASE_URL = 'https://byvs.onrender.com';
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem('authToken');
